f45967323e1e9f81f456f6d24ddac4de8598dd0f
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-03-24 12:25:36 +0530

    C Program To Find The Multiplication Of Two Numbers

---
+++ MultiTab.c
@@ -0,0 +1,56 @@
+/*
+Program: C program to find the multiplication of two numbers entered by the user.
+Date: Wed, 24-3-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/gcc v7.1.1/Vim editor
+*/
+
+// program
+
+#include <stdio.h> // c header file
+#include <stdlib.h>
+
+int main() // main function
+{
+    system ("cls"); // clear the console screen
+    
+    /*
+    for linux / macos, use "clear" command inside the "system ()" function.
+    */
+    
+    printf ("Please enter first number:\n");
+    int num1;
+    scanf ("%d", &num1);
+    
+    printf ("Please enter second number:\n");
+    int num2;
+    scanf ("%d", &num2);
+    
+    // calculate multiplication
+    int result = num1 * num2;
+    
+    // print result
+    printf ("\nThe multiplication of %d and %d is: %d\n", num1, num2, result);
+    
+    printf ("\nThanks for using my application!\n");
+    
+    return 0;
+}
+
+/*
+Save this file as "MultiTab.c"
+compile:
+$ gcc MultiTab.c -o MultiTab [hit Enter]
+execute:
+$ MultiTab [hit Enter]
+output:
+
+Please enter first number:                                                                                                      
+25                                                                                                                              
+Please enter second number:                                                                                                     
+5                                                                                                                               
+                                                                                                                                
+The multiplication of 25 and 5 is: 125                                                                                          
+                                                                                                                                
+Thanks for using my application!
+*/
