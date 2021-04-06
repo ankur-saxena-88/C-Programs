e37fbb3ff0f972169b05790e538fdcc120f28411
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-04-06 19:53:42 +0530

    Area of a Rectangle

--- RectangleArea1.c
+++ RectangleArea1.c
@@ -5,33 +5,45 @@ Date: Tue, 6-4-2021
 Platform: Windows 10 Pro/x64/gcc 8.1/VS Code
 */
 
+// formula to calculate the area of a rectangle, Area = width * height
+
 // program start
-#include <stdio.h> // c header file
-#include <stdlib.h>
+
+#include<stdio.h> // c header file
+#include<stdlib.h>
 
 // main function
 int main()
 {
     system ("cls"); // clear the console screen
     
-    // get input from the user
-    printf ("\nPlease enter the width of the rectangle:\n");
+    printf("\nPlease enter the width of the rectangle:\n");
     double width;
-    scanf("%lf", &width);
+    scanf("%lf",&width);
 
-    printf ("Please enter the height of the rectangle:\n");
+    printf("\nPlease enter the height of the rectangle:\n");
     double height;
-    scanf("%lf", &height);
+    scanf("%lf",&height);
 
     // calculate area of a rectangle
-    double rectangleArea = width * height;
+    double rec_area = width * height;
 
     // print area of a rectangle
-    printf ("\nArea of the rectangle is: %.2lf\n", rectangleArea);
-
-    return 0;
+    printf("\nArea of a rectangle is: %.2lf\n",rec_area);
 }
+
+// program end
 
 // save this file as 'RectangleArea1.c'
 // compile: $ gcc RectangleArea1.c -o RectangleArea1 [hit Enter]
 // execute: $ RectangleArea1 [hit Enter]
+
+/*
+Please enter the width of the rectangle:
+12
+
+Please enter the height of the rectangle:
+9
+
+Area of a rectangle is: 108.00
+*/
